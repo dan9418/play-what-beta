05def1fd63a44b83bc3c59d1f07a0024d967d95b
Dan <dpb34@pitt.edu>
2019-06-18 17:11:45 -0400

    added all inputs

--- Views/Settings/SettingsPanel.js
+++ Views/Settings/SettingsPanel.js
@@ -3,10 +3,7 @@ class SettingsPanel extends React.Component {
 	constructor(props) {
         super(props);
 
-        this.state = {
-            intervals: 'choose',
-            homeDegrees: 'choose'
-		};
+        this.state = {};
     }
 
     update = (name, value) => {
@@ -19,6 +16,16 @@ class SettingsPanel extends React.Component {
         return e('div', { id: 'settings-panel-' + this.props.id },
             e('pre', { id: 'display-text' }, JSON.stringify(this.state)),
             e(SettingsSelect, {
+                id: 'concepts',
+                options: ALL_CONCEPTS,
+                onChange: (event) => { this.update('concepts', event.target.value);} },
+                null),
+            e(SettingsSelect, {
+                id: 'labels',
+                options: ALL_LABELS,
+                onChange: (event) => { this.update('labels', event.target.value);} },
+                null),
+            e(SettingsSelect, {
                 id: 'intervals',
                 options: ALL_INTERVALS,
                 onChange: (event) => { this.update('intervals', event.target.value);} },
@@ -27,6 +34,31 @@ class SettingsPanel extends React.Component {
                 id: 'homeDegrees',
                 options: ALL_HOME_DEGREES,
                 onChange: (event) => { this.update('homeDegrees', event.target.value);} },
+                null),
+            e(SettingsSelect, {
+                id: 'accidentals',
+                options: ALL_ACCIDENTALS,
+                onChange: (event) => { this.update('accidentals', event.target.value);} },
+                null),
+            e(SettingsSelect, {
+                id: 'chords',
+                options: ALL_CHORDS,
+                onChange: (event) => { this.update('chords', event.target.value);} },
+                null),
+            e(SettingsSelect, {
+                id: 'scales',
+                options: ALL_SCALES,
+                onChange: (event) => { this.update('scales', event.target.value);} },
+                null),
+            e(SettingsSelect, {
+                id: 'modes',
+                options: ALL_MODES,
+                onChange: (event) => { this.update('modes', event.target.value);} },
+                null),
+            e(SettingsSelect, {
+                id: 'romanNumerals',
+                options: ALL_ROMAN_NUMERALS,
+                onChange: (event) => { this.update('romanNumerals', event.target.value);} },
                 null)
         );
     };
