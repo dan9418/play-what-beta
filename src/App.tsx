4bc7e68d4b17a4ddce4271d221e0c64ab5de95f8
Dan <dpb34@pitt.edu>
2019-08-11 09:19:43 -0400

    remove multi viewer for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./App.css";
-import { InputPanel } from "./InputPanel/InputPanel";
 import { TheoryEngine } from "./Common/Theory/TheoryEngine";
 import { ViewerType, ALL_VIEWERS } from "./Viewers/ViewerConfig";
 import { ConceptType, ALL_CONCEPTS } from "./Common/Theory/Concepts/ConceptConfig";
 import { Accidental, ACCIDENTAL } from "./Common/Theory/Key/AccidentalConfig";
 import { Degree, DEGREE } from "./Common/Theory/Key/DegreeConfig";
 import { Preset, ConceptConfig } from "./Common/Theory/TheoryConfig";
+import { InputRow } from "./InputPanel/InputRow/InputRow";
 
 export type ViewerProps = {
 	viewerType: ViewerType,
@@ -20,96 +20,56 @@ export type ViewerProps = {
 	concept: Preset<ConceptConfig>,
 }
 
-type AppState = {
-	viewers: ViewerProps[]
-}
-
-export class App extends React.Component<any, AppState> {
+export class App extends React.Component<any, ViewerProps> {
 
 	constructor(props) {
 		super(props);
 
+		const VIEWER_INDEX = 2;
+		const CONCEPT_INDEX = 1;
+
 		this.state = {
-			viewers: [
-				{
-					viewerType: ALL_VIEWERS[2],
-					viewer: ALL_VIEWERS[2].presets[0],
-					viewerConfigPanel: ALL_VIEWERS[2].configComponent,
-					degree: DEGREE.C,
-					accidental: ACCIDENTAL.natural,
-					octave: 4,
-					conceptType: ALL_CONCEPTS[0],
-					concept: ALL_CONCEPTS[0].presets[0]
-				}
-			]
+			viewerType: ALL_VIEWERS[VIEWER_INDEX],
+			viewer: ALL_VIEWERS[VIEWER_INDEX].presets[0],
+			viewerConfigPanel: ALL_VIEWERS[VIEWER_INDEX].configComponent,
+			degree: DEGREE.C,
+			accidental: ACCIDENTAL.natural,
+			octave: 4,
+			conceptType: ALL_CONCEPTS[CONCEPT_INDEX],
+			concept: ALL_CONCEPTS[CONCEPT_INDEX].presets[0]
 		}
 	}
 
-	setValue = (index: number, property: string, value: any) => {
-		/*let update = {};
+	setValue = (property: string, value: any) => {
+		let update = {};
 		update[property] = value;
-		this.setState(update);*/
-		this.setState((oldState) => {
-			let oldViewer = { ...oldState.viewers[index] };
-			oldViewer[property] = value;
-			if (property !== 'viewerType') {
-				return {
-					viewers: [...oldState.viewers.slice(0, index), oldViewer, ...oldState.viewers.slice(index + 1)]
-				};
-			}
-			// temp ineffecient workaround
-			else {
-				oldViewer['viewer'] = ALL_VIEWERS.find((viewer) => { return viewer.id === value.id }).presets[0];
-				return {
-					viewers: [...oldState.viewers.slice(0, index), oldViewer, ...oldState.viewers.slice(index + 1)]
-				};
-			}
-		})
+		this.setState(update);
 	}
 
-	add = () => {
-		this.setState((oldState) => {
-			return {
-				viewers: [...oldState.viewers, oldState.viewers[oldState.viewers.length - 1]]
-			};
-		})
-	}
-
-	getKey = (index: number) => {
+	getKey = () => {
 		return {
-			degree: this.state.viewers[index].degree,
-			accidental: this.state.viewers[index].accidental
+			degree: this.state.degree,
+			accidental: this.state.accidental
 		}
 	}
 
-	getNotes = (index: number) => {
-		let key = this.getKey(index);
-		return TheoryEngine.getNotesFromIntervals(key, this.state.viewers[index].concept.config.intervals, this.state.viewers[index].octave);
-	}
-
-	getViewers = () => {
-		let viewers = []
-		for (let i = 0; i < this.state.viewers.length; i++) {
-			let Viewer = this.state.viewers[i].viewerType.component;
-			viewers.push(
-				<Viewer
-					key={i}
-					notes={this.getNotes(i)}
-					config={this.state.viewers[i].viewer.config}
-				/>);
-		}
-		return viewers;
+	getNotes = () => {
+		let key = this.getKey();
+		return TheoryEngine.getNotesFromIntervals(key, this.state.concept.config.intervals, this.state.octave);
 	}
 
 	render = () => {
+		let Viewer = this.state.viewerType.component;
 		return (
 			<div className='app-container'>
-				<InputPanel
-					viewers={this.state.viewers}
+				<InputRow
+					viewer={this.state}
 					setValue={this.setValue}
-					add={this.add}
 				/>
-				{this.getViewers()}
+				<Viewer
+					notes={this.getNotes()}
+					config={this.state.viewer.config}
+				/>
 			</div>
 		);
 	};
