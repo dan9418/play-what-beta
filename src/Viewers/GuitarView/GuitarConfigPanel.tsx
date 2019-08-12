1a5b3a339692a821e38c9e40cd6632782b2ff43a
Dan <dpb34@pitt.edu>
2019-08-12 17:19:35 -0400

    guitar config ui

--- src/Viewers/GuitarView/GuitarConfigPanel.tsx
+++ src/Viewers/GuitarView/GuitarConfigPanel.tsx
@@ -67,8 +67,8 @@ export class GuitarConfigPanel extends React.Component<GuitarConfigPanelProps, n
         let items = [];
         for (let i = 0; i < this.props.viewerConfig.strings.length; i++) {
             let child = this.props.viewerConfig.strings[i];
-            items.push(<div key={i} className='string'>
-                <div className='input-row-label'>{i}</div>
+            items.push(<div key={i} className='string-row'>
+                <div className='string-label'>{i + 1}</div>
                 <NumericSelector
                     value={this.props.viewerConfig.strings[i].openPosition}
                     setValue={(value) => {
@@ -76,7 +76,8 @@ export class GuitarConfigPanel extends React.Component<GuitarConfigPanelProps, n
                         s[i].openPosition = value;
                         this.props.setValue('strings', s);
                     }} />
-                    </div>);
+                <div className='string-voicings'>*</div>
+            </div>);
         }
         return items;
     }
@@ -84,37 +85,54 @@ export class GuitarConfigPanel extends React.Component<GuitarConfigPanelProps, n
     render = () => {
         return (
             <div className='guitar-config-panel'>
-                <div className='input-row-label'>Fret Label</div>
-                <DropdownSelector
-                    data={ALL_GUITAR_FRET_LABELS}
-                    value={this.props.viewerConfig.fretLabel}
-                    setValue={(value) => { this.props.setValue('fretLabel', value); }} />
-                <div className='input-row-label'>Low Fret</div>
-                <NumericSelector
-                    value={this.props.viewerConfig.fretLow}
-                    setValue={(value) => { this.props.setValue('fretLow', value); }} />
-                <div className='input-row-label'>High Fret</div>
-                <NumericSelector
-                    value={this.props.viewerConfig.fretHigh}
-                    setValue={(value) => { this.props.setValue('fretHigh', value); }} />
-                <div className='input-row-label'>Filter Octave</div>
-                <SwitchSelector
-                    value={this.props.viewerConfig.filterOctave}
-                    setValue={(value) => { this.props.setValue('filterOctave', value); }} />
-                <div className='input-row-label'>Show Dots</div>
-                <SwitchSelector
-                    value={this.props.viewerConfig.showDots}
-                    setValue={(value) => { this.props.setValue('showDots', value); }} />
-                <div className='input-row-label'>Strings</div>
-                <NumericSelector
-                    value={this.props.viewerConfig.strings.length}
-                    setValue={(value) => {
-                        if (value > this.props.viewerConfig.strings.length)
-                            this.props.setValue('strings', [...this.props.viewerConfig.strings, { openPosition: 0 }]);
-                        else
-                            this.props.setValue('strings', this.props.viewerConfig.strings.splice(0, value));
-                    }} />
-                {this.getStringInputs()}
+                <div className='config-panel-header'>Frets</div>
+                <div className='config-panel-row-label'>Label</div>
+                <div className='config-panel-row-contents'>
+                    <DropdownSelector
+                        data={ALL_GUITAR_FRET_LABELS}
+                        value={this.props.viewerConfig.fretLabel}
+                        setValue={(value) => { this.props.setValue('fretLabel', value); }} />
+                </div>
+                <div className='config-panel-row-label'>Low</div>
+                <div className='config-panel-row-contents'>
+                    <NumericSelector
+                        value={this.props.viewerConfig.fretLow}
+                        setValue={(value) => { this.props.setValue('fretLow', value); }} />
+                </div>
+                <div className='config-panel-row-label'>High</div>
+                <div className='config-panel-row-contents'>
+                    <NumericSelector
+                        value={this.props.viewerConfig.fretHigh}
+                        setValue={(value) => { this.props.setValue('fretHigh', value); }} />
+                </div>
+                <div className='config-panel-row-label'>Filter Octave</div>
+                <div className='config-panel-row-contents'>
+                    <SwitchSelector
+                        value={this.props.viewerConfig.filterOctave}
+                        setValue={(value) => { this.props.setValue('filterOctave', value); }} />
+                </div>
+                <div className='config-panel-row-label'>Show Dots</div>
+                <div className='config-panel-row-contents'>
+                    <SwitchSelector
+                        value={this.props.viewerConfig.showDots}
+                        setValue={(value) => { this.props.setValue('showDots', value); }} />
+                </div>
+                <div className='config-panel-header'>Strings</div>
+                <div className='config-panel-row-label'>Count</div>
+                <div className='config-panel-row-contents'>
+                    <NumericSelector
+                        value={this.props.viewerConfig.strings.length}
+                        setValue={(value) => {
+                            if (value > this.props.viewerConfig.strings.length)
+                                this.props.setValue('strings', [...this.props.viewerConfig.strings, { openPosition: 0 }]);
+                            else
+                                this.props.setValue('strings', this.props.viewerConfig.strings.splice(0, value));
+                        }} />
+                </div>
+                <div className='string-config-panel'>
+                    <div className='string-row'><span>#</span><span>Tuning</span><span>Voicings</span></div>
+                    {this.getStringInputs()}
+                </div>
             </div>
         );
     }
