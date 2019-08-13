720fdb70888b73866afea8f510a78dcb04ee4923
Dan <dpb34@pitt.edu>
2019-08-13 19:40:04 -0400

    added roman numeral support

--- src/Concepts/ConceptConfig.ts
+++ src/Concepts/ConceptConfig.ts
@@ -71,17 +71,52 @@ export const CONCEPT_DEFINITIONS: ConceptDefinition[] = [
         name: 'Roman Numerals',
         presets: [
             {
-                id: 'major_1',
-                name: 'Major 1',
+                id: 'I',
+                name: 'I',
                 config: {
                     intervals: [INTERVAL.PU, INTERVAL.M3, INTERVAL.P5]
                 }
             },
             {
-                id: 'major_2',
-                name: 'Major 2',
+                id: 'ii',
+                name: 'ii',
                 config: {
-                    intervals: [INTERVAL.PU, INTERVAL.M3, INTERVAL.P5]
+                    intervals: [INTERVAL.M2, INTERVAL.P4, INTERVAL.M6]
+                }
+            },
+            {
+                id: 'iii',
+                name: 'iii',
+                config: {
+                    intervals: [INTERVAL.M3, INTERVAL.P5, INTERVAL.M7]
+                }
+            },
+            {
+                id: 'IV',
+                name: 'IV',
+                config: {
+                    intervals: [INTERVAL.P4, INTERVAL.M6, Object.assign({ octaveOffset: 1 }, { ...INTERVAL.PU })]
+                }
+            },
+            {
+                id: 'V',
+                name: 'V',
+                config: {
+                    intervals: [INTERVAL.P5, INTERVAL.M7, Object.assign({ octaveOffset: 1 }, { ...INTERVAL.M2 })]
+                }
+            },
+            {
+                id: 'vi',
+                name: 'vi',
+                config: {
+                    intervals: [INTERVAL.M6, Object.assign({ octaveOffset: 1 }, { ...INTERVAL.PU }), Object.assign({ octaveOffset: 1 }, { ...INTERVAL.M3 })]
+                }
+            },
+            {
+                id: 'vii',
+                name: 'vii',
+                config: {
+                    intervals: [INTERVAL.M7, Object.assign({ octaveOffset: 1 }, { ...INTERVAL.M2 }), Object.assign({ octaveOffset: 1 }, { ...INTERVAL.P4 })]
                 }
             }
         ],
