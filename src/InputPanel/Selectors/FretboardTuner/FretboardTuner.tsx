8b5f2c17e3f3541bb26e5dec00004ca41a31210c
Dan <dpb34@pitt.edu>
2019-08-15 15:17:17 -0400

    cleanups

--- src/InputPanel/Selectors/FretboardTuner/FretboardTuner.tsx
+++ src/InputPanel/Selectors/FretboardTuner/FretboardTuner.tsx
@@ -5,6 +5,7 @@ import { GuitarStringConfig, GuitarConfig } from "../../../Viewers/GuitarView/Gu
 import { ViewerProps } from "../../../Viewers/Viewer/Viewer";
 import "./FretboradTuner.css";
 import { string } from "prop-types";
+import { Interval } from "../../../Concepts/Interval/IntervalConfig";
 
 interface FretboardTunerProps extends SelectorProps, ViewerProps {
     viewerConfig: GuitarConfig;
@@ -23,42 +24,60 @@ export class FretboardTuner extends React.Component<FretboardTunerProps, null> {
         this.props.setValue(mergedConfig);
     }
 
+    toggleInterval = (interval: Interval, filteredIntervals: Interval[], stringIndex: number) => {
+        let newIntervals: boolean | Interval[] = false;
+
+        // If currently in "off" state, add all other intervals
+        if (!filteredIntervals) {
+            newIntervals = this.props.conceptIntervals.filter((x) => { return x.id !== interval.id; });
+        }
+        // If interval is already filtered, remove it
+        else if (this.isIntervalFiltered(interval, filteredIntervals)) {
+            newIntervals = filteredIntervals.filter((x) => { return x.id !== interval.id; });
+        }
+        // Otherwise, add it
+        else {
+            newIntervals = [...filteredIntervals];
+            newIntervals.push(interval);
+            // If all intervals are now filtered, set to "off" state for efficiency
+            if (newIntervals.length === this.props.conceptIntervals.length)
+                newIntervals = false;
+        }
+        this.setValue(stringIndex, 'filteredIntervals', newIntervals);
+    }
+
+    isIntervalFiltered = (interval: Interval, filteredIntervals: Interval[]): boolean => {
+        // If in "off" state, no intervals are filtered. Otherwise, check for existence in array.
+        return 'undefined' !== typeof filteredIntervals.find((filterInteval) => { return filterInteval.id === interval.id; });
+    }
+
     getHeaderRow = () => {
-        let items = [<th>#</th>, <th>Tuning</th>];
+        let items = [<th key='header-num'>#</th>, <th key='header-tuning'>Tuning</th>];
         for (let i = 0; i < this.props.conceptIntervals.length; i++) {
             let child = this.props.conceptIntervals[i];
-            items.push(<th>{child.id}</th>);
+            items.push(<th key={child.id}>{child.id}</th>);
         }
         return <tr>{...items}</tr>;
     }
 
     getStringTuner = (stringConfig: GuitarStringConfig, stringIndex: number) => {
         let items = [
-            <td className='string-number'>{stringIndex + 1}</td>,
-            <td><NumericSelector value={stringConfig.openPosition} setValue={(value) => { this.setValue(stringIndex, 'openPosition', value); }} /></td>
+            <td key='num' className='string-number'>{stringIndex + 1}</td>,
+            <td key='tuner'><NumericSelector value={stringConfig.openPosition} setValue={(value) => { this.setValue(stringIndex, 'openPosition', value); }} /></td>
         ];
         for (let i = 0; i < this.props.conceptIntervals.length; i++) {
             let interval = this.props.conceptIntervals[i];
             items.push(
-                <td>
+                <td key={interval.id}>
                     {<input
                         type='checkbox'
-                        checked={
-                            !stringConfig.filteredIntervals
-                            || 'undefined' !== typeof stringConfig.filteredIntervals.find((filterInteval) => { return filterInteval.id === interval.id; })
-                        }
-                        onClick={() => {
-                            let newIntervals = stringConfig.filteredIntervals ? [...stringConfig.filteredIntervals] : [...this.props.conceptIntervals];
-                            let exists = 'undefined' !== typeof newIntervals.find((filterInteval) => { return filterInteval.id === interval.id; });
-                            if (exists) newIntervals = newIntervals.filter((x) => { return x.id !== interval.id; });
-                            else newIntervals.push(interval);
-                            this.setValue(stringIndex, 'filteredIntervals', newIntervals)
-                        }}
+                        checked={!stringConfig.filteredIntervals || this.isIntervalFiltered(interval, stringConfig.filteredIntervals)}
+                        onChange={() => { this.toggleInterval(interval, stringConfig.filteredIntervals, stringIndex); }}
                     />}
                 </td>
             );
         }
-        return <tr>{...items}</tr>;
+        return <tr key={stringIndex}>{...items}</tr>;
     }
 
     getStringTuners = () => {
