592783fe42d627d185391101c7ec71204530557a
Dan <dpb34@pitt.edu>
2019-08-26 16:35:53 -0400

    set up basic jest testing

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    transform: { ".(ts|tsx)": "ts-jest" },
+    testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+    moduleFileExtensions: ["ts", "tsx", "js"]
+};
---
+++ src/Common/Units.test.ts
@@ -0,0 +1,6 @@
+
+import { Utils } from "./Utils";
+
+test('modulo', () => {
+    expect(Utils.modulo(8, 7)).toBe(1);
+});
