4570616d4a5fa9d853fd9ba2f39291d017bf0dfa
Dan <dpb34@pitt.edu>
2019-08-27 16:38:15 -0400

    enum support

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "./Common/TheoryStyles.css";
 import { Fretboard } from "./Components/Fretboard/Fretboard";
 import { Keyboard } from "./Components/Keyboard/Keyboard";
 import { TheoryEngine } from "./Common/TheoryEngine";
-import { INTERVAL_PAIR, INTERVAL, CHORD, SCALE, MODE, DEGREE, ACCIDENTAL } from "./Common/TheoryConstants";
-//import { Concepts } from "./Common/TheoryTypes";
+import { NOTE_LABEL, INTERVAL_PAIR, INTERVAL, CHORD, SCALE, MODE, DEGREE, ACCIDENTAL } from "./Common/TheoryConstants";
 
-export { Fretboard, Keyboard, TheoryEngine, DEGREE, ACCIDENTAL, INTERVAL_PAIR, INTERVAL, CHORD, SCALE, MODE };
+export { Fretboard, Keyboard, TheoryEngine, NOTE_LABEL, DEGREE, ACCIDENTAL, INTERVAL_PAIR, INTERVAL, CHORD, SCALE, MODE };
