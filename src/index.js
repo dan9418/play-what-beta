c1806c4a6e112131d01c2539401ba49c715510ec
Dan <dpb34@pitt.edu>
2019-09-01 14:44:44 -0400

    add some

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import "./Common/TheoryStyles.css";
 import { Tonic, Accidental, Interval, ConceptPreset, PhysicalNote, FunctionalNote, CompleteNote } from "./Common/TheoryTypes";
 import { NOTE_LABEL, INTERVAL, MAJOR_SCALE, CALIBRATION_NOTE, TONIC, ACCIDENTAL, INTERVAL_PAIR, CHORD, SCALE, MODE, ROMAN_NUMERAL } from "./Common/TheoryConstants";
-import { FretboardProps, DEFAULT_FRETBOARD_PROPS } from "./Components/Fretboard/Fretboard";
+import { Fretboard, FretboardProps, DEFAULT_FRETBOARD_PROPS } from "./Components/Fretboard/Fretboard";
 import { FretboardStringConfig } from "./Components/Fretboard/FretboardString";
-import { KeyboardProps, DEFAULT_KEYBOARD_PROPS } from "./Components/Keyboard/Keyboard";
+import { Keyboard, KeyboardProps, DEFAULT_KEYBOARD_PROPS } from "./Components/Keyboard/Keyboard";
 import { withNotes, ViewerProps, KeyCenter, DEFAULT_KEY_CENTER, Concept, DEFAULT_CONCEPT } from "./Components/withNotes";
 
 export {
     Tonic, Accidental, Interval, ConceptPreset, PhysicalNote, FunctionalNote, CompleteNote,
     NOTE_LABEL, INTERVAL, MAJOR_SCALE, CALIBRATION_NOTE, TONIC, ACCIDENTAL, INTERVAL_PAIR, CHORD, SCALE, MODE, ROMAN_NUMERAL,
-    FretboardProps, DEFAULT_FRETBOARD_PROPS,
+    Fretboard, FretboardProps, DEFAULT_FRETBOARD_PROPS,
     FretboardStringConfig,
-    KeyboardProps, DEFAULT_KEYBOARD_PROPS,
+    Keyboard, KeyboardProps, DEFAULT_KEYBOARD_PROPS,
     withNotes, ViewerProps, KeyCenter, DEFAULT_KEY_CENTER, Concept, DEFAULT_CONCEPT
 };
